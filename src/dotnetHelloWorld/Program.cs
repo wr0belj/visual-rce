8073b23d84067b592ff62f669ed12c88985e70ba
wr0belj <jakub@wrobelj.com>
2023-12-11 22:45:47 +0100

    add project with metasploit payload

---
+++ src/dotnetHelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+
+namespace dotnetHelloWorld
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+             Console.WriteLine("pwned!");
+				}
+    }
+}
